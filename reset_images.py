6e12846116eb93b2d70fb802eb4ab7c313042be2
Bruno Dantas <bruno.d.paiva@hotmail.com>
2022-12-30 16:41:48 -0300

    feat(reset_images.py): add reset images script

---
+++ reset_images.py
@@ -0,0 +1,12 @@
+import os
+
+def reset_images():
+    """This method deletes all the images in the images folder."""
+    for root, dirs, files in os.walk('images'):
+        for archive in files:
+            if archive.endswith('.png'):
+                os.remove(os.path.join(root, archive))
+    
+
+if __name__ == '__main__':
+    reset_images()
