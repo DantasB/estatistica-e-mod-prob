6d4c5106ff9f54e3bcebc0a9f2dce822f048a751
Bruno Dantas <bruno.d.paiva@hotmail.com>
2023-01-02 14:06:16 -0300

    refactor(reset_images.py): remove unused variable

--- src/scripts/reset_images.py
+++ src/scripts/reset_images.py
@@ -2,7 +2,7 @@ import os
 
 def reset_images():
     """This method deletes all the images in the images folder."""
-    for root, dirs, files in os.walk('images'):
+    for root, _, files in os.walk('images'):
         for archive in files:
             if archive.endswith('.png'):
                 os.remove(os.path.join(root, archive))
